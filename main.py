aa866c63ded94255c5d1f5a88600272d972f08a3
timecra2 <79523361+timecra2@users.noreply.github.com>
2024-11-06 02:11:37 +0900

    Harmonics Array tested

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ import numpy as np
 import math
 
 def sinewave(frequency, second, sample_rate=22050,amplitude=1):
-    return np.array([ amplitude * math.sin(frequency * 2 * math.pi *(x / sample_rate)) for x in range(0,second * sample_rate)])
+    return np.array([ (amplitude * math.sin(frequency * 2 * math.pi *(x / sample_rate))) for x in range(0,second * sample_rate)])
+
+def decibel(x):
+    return pow(10,x/10)
 
 sample_rate = 22050
 
@@ -24,14 +27,11 @@ C6 = A4 * pow(2,15/12)
 movingman = pydub.AudioSegment.from_file("movingman.mp3")
 
 
-note = np.concatenate([sinewave(C5,1),
-                       sinewave(D5,1),
-                       sinewave(E5,1),
-                       sinewave(F5,1),
-                       sinewave(G5,1),
-                       sinewave(A6,1),
-                       sinewave(B6,1),
-                       sinewave(C6,1)],axis=None)
+freq_specturm = [A4*x for x in range(1,10)]
+# https://www.youtube.com/watch?v=BvUMfnQucP4
+amp_violin = [2.189,1.256,0.459,0.182,0.161,0.016,0.086,0.024,0.011,0.020]
+
+note = sum(sinewave(frequency=freq,second=2,amplitude=amp) for freq, amp in zip(freq_specturm,amp_violin))
 
 sounddevice.play(data=note,samplerate=sample_rate)
 sounddevice.wait()
