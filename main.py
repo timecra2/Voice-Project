8984fa355cf462d0b590e590f7b59de9f59c1293
timecra2 <79523361+timecra2@users.noreply.github.com>
2024-11-30 21:15:44 +0900

    classification & instruments added

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from pydub import AudioSegment
 import numpy as np
 import math
 
-#https://www.youtube.com/watch?v=DNKaIe3VTy4
+# references
+# https://www.youtube.com/watch?v=DNKaIe3VTy4
+# https://www.youtube.com/watch?v=BvUMfnQucP4
+
 
 def sinewave(frequency, second, sample_rate,amplitude):
     return np.array([ (amplitude * math.sin(frequency * 2 * math.pi *(x / sample_rate))) for x in range(0,second * sample_rate)])
@@ -12,64 +15,75 @@ def decibel(x):
     return pow(10,x/10)
 
 
+#(amplitude,t) -> (amplitude,f)
+class Instrument:
+    def __init__(self,filename):
+        instAudio = AudioSegment.from_file(filename)   
+        instArray = np.array(instAudio.get_array_of_samples())
+        instArray = instArray.reshape(-1,instAudio.channels).mean(axis=1)
+        instArray /= np.max(np.abs(instArray))
+
+        instDFT = np.fft.fft(instArray)
+        instFreq = np.fft.fftfreq(len(instArray),1/instAudio.frame_rate)
+        instMag =np.abs(instDFT)
+
+        instFreqNyq = instFreq[:len(instFreq)//2]
+        instMagNyq = instMag[:len(instMag)//2]
+        instMagNyq /= instMagNyq.max()
+        
+        plt.plot(instFreqNyq,instMagNyq)
+        plt.show()
+
+        freqMagTuples = [(i,j) for i,j in zip(instFreqNyq,instMagNyq)]
+        freqMagDict = {}
+        for tup in freqMagTuples:
+            if round(tup[0],1) in freqMagDict:
+                freqMagDict[round(tup[0],1)] += tup[1]
+            else :
+                freqMagDict[round(tup[0],1)] = tup[1]
+        freqMagDict = dict(sorted(freqMagDict.items(),key = lambda item : item[1],reverse=True))
+
+        self.framerate = instAudio.frame_rate
+        self.instDict = freqMagDict
+
+    def instMap(self,precision):
+        instMap = []
+        for idx,key in enumerate(self.instDict):
+            if(idx >= precision):
+                break
+            instMap.append((key,self.instDict[key]))
+        return instMap
+
+    def writeChart(self,filename):
+        with open(filename,'w') as f:
+            magMax = max(self.instDict.values())
+            for idx,key  in enumerate(self.instDict):
+                f.write(f"{key} {self.instDict[key]/magMax}\n")
+
+    def note(self,freq=1,second=1,amplitude=1,precision=10):
+        return sum(sinewave(frequency=instFreq,
+                            second=second,
+                            amplitude=instAmp*amplitude,
+                            sample_rate=self.framerate) for instFreq,instAmp in self.instMap(precision))
 
 
-violinAudio = AudioSegment.from_file("violin.mp3")
-violinArray = np.array(violinAudio.get_array_of_samples())
-violinArray = violinArray.reshape(-1,violinAudio.channels).mean(axis=1)
-violinArray = violinArray.astype(np.float32)
-violinArray /= np.max(np.abs(violinArray))
-
-audioLength = len(violinArray)/violinAudio.frame_rate
-
-violinDFT = np.fft.fft(violinArray)
-violinFreq = np.fft.fftfreq(len(violinArray),1/violinAudio.frame_rate)
-violinMag = np.abs(violinDFT)
-
-violinFreqNyq = violinFreq[:len(violinFreq)//2]
-violinMagNyq = violinMag[:len(violinMag)//2]
-bin_width = violinFreq[1] - violinFreq[0]
-
-plt.plot(violinFreqNyq,violinMagNyq)
-plt.show()
-
-
-freqMagTuples = [(i,j) for i,j in zip(violinFreqNyq,violinMagNyq)]
-freqMagDict = {}
-
-
-for tup in freqMagTuples:
-    if round(tup[0],1) in freqMagDict:
-        freqMagDict[round(tup[0],1)] += tup[1]
-    else :
-        freqMagDict[round(tup[0],1)] = tup[1]
-
-
-freqMagDict = dict(sorted(freqMagDict.items(),key = lambda item : item[1],reverse=True))
-
-amp_violin = []
-freq_violin = []
 
 
 
-for idx,key in enumerate(freqMagDict):
-    if(idx >= 100):
-        break
-    amp_violin.append(freqMagDict[key])
-    freq_violin.append(key)
 
-amp_violin /= amp_violin[0]
 
-note = sum(sinewave(frequency=freq,second=1,amplitude=amp,sample_rate=44100) for freq,amp in zip(freq_violin,amp_violin))
+acord = Instrument("inst/acord.mp3")
 
-sounddevice.play(note)
+
+
+sounddevice.play(acord.note(second=5,amplitude=0.1,precision=100))
 sounddevice.wait()
+acord.writeChart("acord.csv")
 
-if(True):
-    with open("mag.csv",'w') as f:
-        magLargest = list(freqMagDict.values())[0]
-        for idx,key in enumerate(freqMagDict):
-            f.write(f"{key} {freqMagDict[key]/magLargest}\n")
+
+
+
+
 
 
 
@@ -85,7 +99,7 @@ if(False):
     B6 = A4 * pow(2,14/12)
     C6 = A4 * pow(2,15/12)
     freq_specturm = [A4*x for x in range(1,10)]
-    # https://www.youtube.com/watch?v=BvUMfnQucP4
+
     amp_violin = [2.189,1.256,0.459,0.182,0.161,0.016,0.086,0.024,0.011,0.020]
 
     note = sum(sinewave(frequency=freq,second=2,amplitude=amp) for freq, amp in zip(freq_specturm,amp_violin))
